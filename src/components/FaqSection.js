30d775560e52ab5a213f02edc278b64615529ca2
Sagi Isaschar <sagi536@gmail.com>
2020-12-01 14:29:36 -0500

    3. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FQA</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            quibusdam ab ipsam porro eligendi earum sunt in excepturi enim ad?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            quibusdam ab ipsam porro eligendi earum sunt in excepturi enim ad?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diferrent Payment Method</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            quibusdam ab ipsam porro eligendi earum sunt in excepturi enim ad?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Product do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            quibusdam ab ipsam porro eligendi earum sunt in excepturi enim ad?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;

02ed137ae7b059b79cb6258399d0df7ce2bce8c7
Sagi Isaschar <sagi536@gmail.com>
2020-12-09 09:41:20 -0500

    5. styled services

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import home1 from "../img/home1.png";
 // Import styled
 import styled from "styled-components";
+import { StyledAbout, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
@@ -31,38 +32,5 @@ const AboutSection = () => {
     </StyledAbout>
   );
 };
-
-// styled components
-
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;

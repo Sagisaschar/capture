ae64cc6f35e060c60308019f80184a8fc8bdddcc
Sagi Isaschar <sagi536@gmail.com>
2020-12-22 15:23:31 +0200

    7. our work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetails from "./pages/MovieDetails";
 import { Route, Switch } from "react-router-dom";
 // Global style
 import GlobalStyle from "./components/GlobalStyle";
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetails />
         </Route>
         <Route path="/contact-us">
           <ContactUs />
